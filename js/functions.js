d2666fb563e8c21c07a48a8f316c537ffeee643b
Inna <pushkarevain22@gmail.com>
2023-10-11 01:46:02 +0500

    M2T1-changes-in-functions

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,13 @@
 /* eslint-disable no-console */
 
-function checkStringLength(inputString, maxLength) {
-  return inputString.length <= maxLength;
-}
+const checkStringLength = (inputString, maxLength) => inputString.length <= maxLength;
 console.log(checkStringLength('проверяемая строка', 20));//true
-console.log(checkStringLength('проверяемая строка', 18));//true
 console.log(checkStringLength('проверяемая строка', 10));//false
 
 function isPalindrome(inputString) {
   const cleanString = inputString.replace(/\s/g, '').toLowerCase();
   return cleanString === cleanString.split('').reverse().join('');
 }
-console.log(isPalindrome('топот'));//true
 console.log(isPalindrome('ДовОд'));//true
 console.log(isPalindrome('Кекс'));//false
 console.log(isPalindrome('Лёша на полке клопа нашёл '));// true
